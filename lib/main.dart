0a583d6b189f13890c5b8df11e5c605c84fd1b75
athul johny <athuljohny123@gmail.com>
2022-11-18 00:07:06 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,258 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: BlogHomeOnePage(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+final List<Map> articles = [
+  {
+    "title": "How to Seem Like You Always Have Your Shot Together",
+    "author": "Jonhy Vino",
+    "time": "4 min read",
+  },
+  {
+    "title": "Does Dry is January Actually Improve Your Health?",
+    "author": "Jonhy Vino",
+    "time": "4 min read",
+  },
+  {
+    "title": "You do hire a designer to make something. You hire them.",
+    "author": "Jonhy Vino",
+    "time": "4 min read",
+  },
+  {
+    "title": "How to Seem Like You Always Have Your Shot Together",
+    "author": "Jonhy Vino",
+    "time": "4 min read",
+  },
+  {
+    "title": "How to Seem Like You Always Have Your Shot Together",
+    "author": "Jonhy Vino",
+    "time": "4 min read",
+  },
+  {
+    "title": "Does Dry is January Actually Improve Your Health?",
+    "author": "Jonhy Vino",
+    "time": "4 min read",
+  },
+  {
+    "title": "You do hire a designer to make something. You hire them.",
+    "author": "Jonhy Vino",
+    "time": "4 min read",
+  },
+  {
+    "title": "How to Seem Like You Always Have Your Shot Together",
+    "author": "Jonhy Vino",
+    "time": "4 min read",
+  },
+];
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class BlogHomeOnePage extends StatelessWidget {
+  final Color primaryColor = const Color(0xfED22192);
+  final Color bgColor = const Color(0xffa9E0E3);
+  final Color secondaryColor = const Color(0xff324558);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 5,
+      child: Theme(
+        data: ThemeData(
+          primaryColor: primaryColor,
+          appBarTheme: AppBarTheme(
+            color: Colors.white,
+            iconTheme: IconThemeData(color: secondaryColor),
+            actionsIconTheme: IconThemeData(
+              color: secondaryColor,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            toolbarTextStyle: TextTheme(
+              subtitle1: TextStyle(
+                color: secondaryColor,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ).bodyText2,
+            titleTextStyle: TextTheme(
+              subtitle1: TextStyle(
+                color: secondaryColor,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ).headline6,
+          ),
+        ),
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text(
+              'Categories',
+              style: TextStyle(color: Colors.black),
             ),
-          ],
+            leading: const Icon(Icons.category),
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {},
+              )
+            ],
+            bottom: TabBar(
+              isScrollable: true,
+              labelColor: primaryColor,
+              indicatorColor: primaryColor,
+              unselectedLabelColor: secondaryColor,
+              tabs: const <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("For You"),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("Design"),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("Beauty"),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("Education"),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("Entertainment"),
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              ListView.separated(
+                padding: const EdgeInsets.all(16.0),
+                itemCount: articles.length,
+                itemBuilder: (context, index) {
+                  return _buildArticleItem(index);
+                },
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 16.0),
+              ),
+              const Text("Tab 2"),
+              const Text("Tab 3"),
+              const Text("Tab 4"),
+              const Text("Tab 5"),
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: 1,
+            type: BottomNavigationBarType.fixed,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: "",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.folder),
+                label: "",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite_border),
+                label: "",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline),
+                label: "",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: "",
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _buildArticleItem(int index) {
+    Map article = articles[index];
+    return Container(
+      color: Colors.white,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: 90,
+            height: 90,
+            color: bgColor,
+          ),
+          Container(
+            color: Colors.white,
+            padding: const EdgeInsets.all(16.0),
+            margin: const EdgeInsets.all(16.0),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  color: Colors.blue,
+                  width: 80.0,
+                  child: const Image(
+                    image: NetworkImage(
+                      "https://static.vecteezy.com/system/resources/previews/000/201/215/original/vector-beach-vacations-background.jpg",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(width: 20.0),
+                Expanded(
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        article["title"],
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          color: secondaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                      Text.rich(
+                        TextSpan(
+                          children: [
+                            WidgetSpan(
+                              child: CircleAvatar(
+                                radius: 15.0,
+                                backgroundColor: primaryColor,
+                              ),
+                            ),
+                            const WidgetSpan(
+                              child: SizedBox(width: 5.0),
+                            ),
+                            TextSpan(
+                                text: article["author"],
+                                style: const TextStyle(fontSize: 16.0)),
+                            const WidgetSpan(
+                              child: SizedBox(width: 20.0),
+                            ),
+                            const WidgetSpan(
+                              child: SizedBox(width: 5.0),
+                            ),
+                            TextSpan(
+                              text: article["time"],
+                            ),
+                          ],
+                        ),
+                        style: const TextStyle(height: 2.0),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
